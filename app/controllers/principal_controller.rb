4a4737c24c74c1faa26c8677a91b92f61044e66c
christianrogz <christian_rogz94@hotmail.com>
2016-07-07 13:37:54 -0500

    Cambios para busqueda

--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -2,4 +2,9 @@ class PrincipalController < ApplicationController
   def index
   	@registros = Principal.where(:estado => "impreso")
   end
+  def finder
+  	@usuario = params[:usuario]
+  	@carrera = params[:carrera]
+  	@registros = Principal.where(:usuario => @usuario, :carrera => @carrera, :estado => "impreso")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,13 @@ Rails.application.routes.draw do
 
   get 'progress/index'
 
-  get 'principal/index'
+  #get 'principal/index'
+
+  #get 'principal/finder'
+
+  get 'principal/index' => 'principal#index'
+
+  post 'principal/index' => 'principal#finder'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
